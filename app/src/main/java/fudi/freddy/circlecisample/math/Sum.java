6888608c08c6b19116b43a42cfb7bacc072baa59
Freddyfudi <romllz489@gmail.com>
2018-02-10 15:25:54 -0500

    Archivos basicos

---
+++ app/src/main/java/fudi/freddy/circlecisample/math/Sum.java
@@ -0,0 +1,14 @@
+package fudi.freddy.circlecisample.math;
+
+/**
+ * @version 1.0
+ * @autor Romell Domínguez
+ * @date 2/10/18
+ */
+
+public class Sum {
+
+    public static final int method(int a ,int b){
+        return a + b;
+    }
+}
---
+++ app/src/test/java/fudi/freddy/circlecisample/math/SumTest.java
@@ -0,0 +1,24 @@
+package fudi.freddy.circlecisample.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @version 1.0
+ * @autor Romell Domínguez
+ * @date 2/10/18
+ */
+public class SumTest {
+    @Test
+    public void method() throws Exception {
+        assertEquals(Sum.method(2,3),5);
+    }
+
+
+    @Test
+    public void methodError() throws Exception {
+        assertEquals(Sum.method(2,3),6);
+    }
+
+}
